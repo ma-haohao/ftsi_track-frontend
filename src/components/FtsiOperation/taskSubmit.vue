71317438378f1ded6484900d7dd51be399617efc
mahaohao0001 <8448896+mahaohao0001@user.noreply.gitee.com>
2021-01-07 13:48:31 +0800

    add files

--- src/components/FtsiOperation/taskSubmit.vue
+++ src/components/FtsiOperation/taskSubmit.vue
@@ -16,18 +16,20 @@
       <el-row :gutter="10">
         <el-col :span="12">
           <el-card>
-            <h3>LHE: {{leftEngineForm.engineNum}}</h3>
-            <el-form ref="leftEngineRef" :model="leftEngineForm" label-width="130px" :rules="leftEngineRules">
+            <h3>LHE: {{ leftEngineForm.engineNum }}</h3>
+            <el-form ref="leftEngineRef" :model="leftEngineForm" label-width="130px" :rules="EngineRules">
               <el-form-item label="Implement FTSI" prop="implementFTSI">
-                <el-input v-model="leftEngineForm.implementFTSI" placeholder="The number of the implemented FTSIs only, separated by  ' / ' "></el-input>
+                <el-input v-model="leftEngineForm.implementFTSI"
+                          placeholder="The number of the FTSIs, separated by  ' / ' "></el-input>
               </el-form-item>
               <el-form-item label="Date">
-                <el-date-picker type="date" placeholder="Choose the implement date" style="width:240px" format="yyyy-MM-dd"
+                <el-date-picker type="date" placeholder="Choose the implement date" style="width:240px"
+                                format="yyyy-MM-dd"
                                 value-format="yyyy-MM-dd"></el-date-picker>
               </el-form-item>
               <el-form-item>
-                <el-button type="warning" @click="checkFTSIinfo(leftEngineForm)">Check</el-button>
-                <el-button type="primary" @click="submitFTSIinfo(leftEngineForm)">Submit</el-button>
+                <el-button type="warning" @click="checkFTSIinfo(leftEngineForm,$refs.leftEngineRef)">Check</el-button>
+                <el-button type="primary" @click="submitFTSIinfo(leftEngineForm,$refs.leftEngineRef)">Submit</el-button>
               </el-form-item>
             </el-form>
             <span style="width: 100%">
@@ -45,20 +47,33 @@
         </el-col>
         <el-col :span="12">
           <el-card>
-            <h3>RHE: {{rightEngineForm.engineNum}}</h3>
-            <el-form ref="rightEngineRef" :model="leftEngineForm" label-width="120px">
-              <el-form-item label="Implement FTSI">
-                <el-input v-model="rightEngineForm.implementFTSI" placeholder="FTSI number only, separated by  ' / ' "></el-input>
+            <h3>RHE: {{ rightEngineForm.engineNum }}</h3>
+            <el-form ref="rightEngineRef" :model="rightEngineForm" label-width="130px" :rules="EngineRules">
+              <el-form-item label="Implement FTSI" prop="implementFTSI">
+                <el-input v-model="rightEngineForm.implementFTSI"
+                          placeholder="The number of the FTSIs, separated by  ' / ' "></el-input>
               </el-form-item>
               <el-form-item label="Date">
-                <el-date-picker type="date" placeholder="Choose the implement date" style="width:240px" format="yyyy-MM-dd"
+                <el-date-picker type="date" placeholder="Choose the implement date" style="width:240px"
+                                format="yyyy-MM-dd"
                                 value-format="yyyy-MM-dd"></el-date-picker>
               </el-form-item>
               <el-form-item>
-                <el-button type="warning">Check</el-button>
-                <el-button type="primary">Submit</el-button>
+                <el-button type="warning" @click="checkFTSIinfo(rightEngineForm,$refs.rightEngineRef)">Check</el-button>
+                <el-button type="primary" @click="submitFTSIinfo(rightEngineForm,$refs.rightEngineRef)">Submit</el-button>
               </el-form-item>
             </el-form>
+            <span style="width: 100%">
+              <el-table :data="rightCheckResult" border stripe
+                        :header-cell-style="{backgroundColor:'#6BA4FD', color:'#ffffff'}" style="font-size: 13px">
+                <el-table-column label="FTSI Num." prop="ftsi_num,rev" width="110">
+                  <template slot-scope="scope"> {{ scope.row.ftsi_num }} Rev. {{ scope.row.rev }}
+                  </template>
+                </el-table-column>
+                <el-table-column label="FTSI Title" prop="ftsi_title"></el-table-column>
+                <el-table-column label="Comments" prop="comments"></el-table-column>
+              </el-table>
+            </span>
           </el-card>
         </el-col>
       </el-row>
@@ -87,8 +102,8 @@ export default {
       }
     }
     return {
-      dialogVisible:false,
-      attentionInfo:'',
+      dialogVisible: false,
+      attentionInfo: '',
       queryForm: {
         aircraftMSN: ''
       },
@@ -101,9 +116,9 @@ export default {
         engineNum: '',
         implementFTSI: ''
       },
-      leftCheckResult:[],
-      rightCheckResult:[],
-      leftEngineRules:{
+      leftCheckResult: [],
+      rightCheckResult: [],
+      EngineRules: {
         implementFTSI: [
           {required: true, message: 'please enter the FTSI number', trigger: 'blur'},
           {validator: checkonlyNumRules, trigger: 'blur'}
@@ -117,40 +132,55 @@ export default {
       if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
       this.aircraftList = res.data.aircraft_list
     },
-    async getEngineNum(){
-      const {data:res}=await this.$http.get('ftsiOpt/getEngineNum/',{params:this.queryForm})
-      if(res.meta.status!==200){return this.$message.error('failed to obtain the engine information')}
-      this.leftEngineForm.engineNum=res.data.left_engine
-      this.rightEngineForm.engineNum=res.data.right_engine
+    async getEngineNum() {
+      const {data: res} = await this.$http.get('ftsiOpt/getEngineNum/', {params: this.queryForm})
+      if (res.meta.status !== 200) {
+        return this.$message.error('failed to obtain the engine information')
+      }
+      this.leftEngineForm.engineNum = res.data.left_engine
+      this.rightEngineForm.engineNum = res.data.right_engine
     },
-    async checkFTSIinfo(engineForm){
-      if(engineForm.engineNum===''){return this.$message.error('Please select the aircraft first.')}
-      this.$refs.leftEngineRef.validate(async valid => {
+    async checkFTSIinfo(engineForm,validateRef) {
+      if (engineForm.engineNum === '') {
+        return this.$message.error('Please select the aircraft first.')
+      }
+      validateRef.validate(async valid => {
         if (!valid) return
         //可以发起添加请求
-        const {data:res}=await this.$http.get('ftsiOpt/checkFTSIinfo/',{params:engineForm})
+        const {data: res} = await this.$http.get('ftsiOpt/checkFTSIinfo/', {params: engineForm})
         console.log(res)
-        this.leftCheckResult=res.data.FTSI_info
-        if (res.data.warning_info!==""){this.attentionDialogOpen(res.data.warning_info)}
+        if (engineForm.engineNum === this.leftEngineForm.engineNum) {
+          this.leftCheckResult = res.data.FTSI_info
+        }else{this.rightCheckResult = res.data.FTSI_info}
+
+        if (res.data.warning_info !== "") {
+          this.attentionDialogOpen(res.data.warning_info)
+        }
       })
     },
-    async submitFTSIinfo(engineForm){
-      if(engineForm.engineNum===''){return this.$message.error('Please select the aircraft first.')}
-      this.$refs.leftEngineRef.validate(async valid => {
+    async submitFTSIinfo(engineForm,validateRef) {
+      if (engineForm.engineNum === '') {
+        return this.$message.error('Please select the aircraft first.')
+      }
+      validateRef.validate(async valid => {
         if (!valid) return
         //可以发起添加请求
-        const {data:res}=await this.$http.get('ftsiOpt/submitFTSIinfo/',{params:engineForm})
-        if (res.meta.status!==200){return this.$message.error('failed to submit the info')}
+        const {data: res} = await this.$http.get('ftsiOpt/submitFTSIinfo/', {params: engineForm})
+        if (res.meta.status !== 200) {
+          return this.$message.error('failed to submit the info')
+        }
         this.$message.success(res.meta.msg)
-        if (res.data.warning_info!==""){this.attentionDialogOpen(res.data.warning_info)}
+        if (res.data.warning_info !== "") {
+          this.attentionDialogOpen(res.data.warning_info)
+        }
       })
     },
-    attentionDialogOpen(info){
-      this.attentionInfo=info
-      this.dialogVisible=true
+    attentionDialogOpen(info) {
+      this.attentionInfo = info
+      this.dialogVisible = true
     },
-    attentionDialogClose(){
-      this.dialogVisible=false
+    attentionDialogClose() {
+      this.dialogVisible = false
     }
   }
 }
